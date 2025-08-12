478be457d5920e25d5ff1ef43488e4a116b11e93
김세진 <124812852+seje06@users.noreply.github.com>
2025-08-12 18:10:54 +0900

    Update OnnxMaker.py

--- Source/FightingGame/python/OnnxMaker.py
+++ Source/FightingGame/python/OnnxMaker.py
@@ -5,17 +5,18 @@ from Dungeon_Final_A2C import ActorCritic
 # 모델을 CPU로 로드하고 inference 모드로 설정
 device = torch.device("cpu")
 model = ActorCritic().to(device)
-checkpoint = torch.load("../SavedModels/Dungeon_A2C/소소전투무/ckpt.pth", map_location=device)
+checkpoint = torch.load("../SavedModels/Dungeon_A2C/소소전투무/ckpt.pth", map_location=device) #학습된 모델 불러오기
 model.load_state_dict(checkpoint["network"])
 model.eval()  # inference 모드로 설정
 
 # 모델을 입력 데이터에 맞게 설정
 # ex) 입력 차원 1, 크기 5인 경우:(1,5)
 dummy_input = torch.randn(1,3)
-onnx_path = "C:/Users/sqlwh/OneDrive/문서/GitHub/Capstone_Unity-Network/Assets/OnnxModels/MoveTest2.onnx"
+onnx_path = "" #생성할 경로
 input_names = ["x"]  # 입력 노드의 이름 지정
 output_names = ["y"]  # 출력 노드의 이름 지정
 export_params=True
 do_constant_folding=True
 #Onnx파일 생성
 torch.onnx.export(model, dummy_input,onnx_path,opset_version=10,input_names = ["x"],output_names = ["y"])
+
